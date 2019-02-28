edec955772a0d80142e56a6b9dab0b1bfc073e14
EC2 Default User <ec2-user@ip-172-31-14-141.us-east-2.compute.internal>
2019-02-28 16:21:09 +0000

    production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-if Rails.env == 'development'
+if Rails.env == 'production'
   
   User.create!(name:  "Test User",
              email: "example@railstutorial.org",
